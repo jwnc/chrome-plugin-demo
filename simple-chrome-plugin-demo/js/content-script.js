44ce7b902c7bc449b32f3fd78aa80f9e98793ae0
wnc <529801034@qq.com>
2019-03-14 23:07:08 +0800

    添加翻译控制, 动态修改图标, 注意两个图标大小应该一致

--- simple-chrome-plugin-demo/js/content-script.js
+++ simple-chrome-plugin-demo/js/content-script.js
@@ -2,21 +2,36 @@
   var str = window.getSelection().toString();
   //window.getSelection().collapseToEnd(); //取消选择
   str = str.replace(/\s+$/, '');
-  if(str && /^[a-z]+$/i.test(str)){
-    console.log('选中单词并复制.'+str);
-	chrome.extension.sendMessage({uri:"xdd_add",data:{},url:"http://localhost:8088/readlog/readlog?word="+str}, function(response) {
-                console.info(response);
-				var mean = response.cn_mean;
-				if(mean){
-					msg = mean.replace('\n', '<br>');
-				}
-				else{
-					msg= '<font color="red">未找到该单词.</font>';
-				}
-				layer.alert(msg,{title: str, shadeClose: true,closeBtn: 1, btn:['详情'], detail: function (index) { 
-					window.open('http://www.iciba.com/'+str);
-					layer.close(index);
-				}});
-            });
-  }
+  
+  tranIfCfg(str);
 });
+
+function tranIfCfg(word){
+	chrome.storage.sync.get({ITtran: true}, function(items) {
+		console.log('tran:'+items.ITtran);
+		if(items.ITtran){
+			tranWithWord(word);
+		}
+	});
+}
+
+function tranWithWord(word){
+	if(word && /^[a-z]+$/i.test(word)){
+		console.log('选中单词.'+word);
+		chrome.extension.sendMessage({uri:"xdd_add",data:{},url:"http://localhost:8088/readlog/readlog?word="+word}, function(response) {
+			console.info(response);
+			var mean = response.cn_mean;
+			if(mean){
+				msg = mean.replace('\n', '<br>');
+			}
+			else{
+				msg= '<font color="red">未找到该单词.</font>';
+			}
+			layer.alert(msg,{title: word, shadeClose: true,closeBtn: 1, btn:['详情'], detail: function (index) { 
+				window.open('http://www.iciba.com/'+word);
+				layer.close(index);
+			}});
+		});
+  }
+}
+
--- simple-chrome-plugin-demo/js/popup.js
+++ simple-chrome-plugin-demo/js/popup.js
@@ -1,1 +1,29 @@
-﻿console.log('你好，我是popup！');
+﻿$(function() {
+	chrome.storage.sync.get({ITtran: true}, function(items) {
+		//document.body.style.backgroundColor = 'black';
+		showbtnTip(items.ITtran);
+	});
+});
+
+$('#changeTranCfg').click(e => {
+	chrome.storage.sync.get({ITtran: true}, function(items) {
+		var ITtran = items.ITtran;
+		//alert('old:'+ITtran);
+		var newITtran = !ITtran;
+		chrome.storage.sync.set({ITtran: newITtran}, function() {
+			//alert('保存成功! new:'+!ITtran);
+			showbtnTip(newITtran);
+		});
+	});
+	
+});
+
+function showbtnTip(flag){
+	$('#changeTranCfg').val(flag ? '正在使用' : '已经禁用');
+	var iconPath = flag ? 'icon.png' : 'icon2.png';
+	chrome.browserAction.setIcon({path : {
+		"48": iconPath,
+		"64": iconPath,
+		"128": iconPath
+	}});
+}
