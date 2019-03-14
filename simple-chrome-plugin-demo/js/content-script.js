a97ec3d01925a8fc9ade4891ddf374b481267172
nengcai.wang <nengcai.wang@ucarinc.com>
2019-03-14 14:07:09 +0800

    添加正则验证, 只查纯英文单词

--- simple-chrome-plugin-demo/js/content-script.js
+++ simple-chrome-plugin-demo/js/content-script.js
@@ -1,10 +1,11 @@
 ﻿$(document).dblclick(function(){
   var str = window.getSelection().toString();
   //window.getSelection().collapseToEnd(); //取消选择
-  if(str.length > 0){
+  str = str.replace(/\s+$/, '');
+  if(str && /^[a-z]+$/i.test(str)){
     console.log('选中单词并复制.'+str);
 	chrome.extension.sendMessage({uri:"xdd_add",data:{},url:"http://localhost:8088/readlog/readlog?word="+str}, function(response) {
-                console.info("xdd_add",response);
+                console.info(response);
 				var mean = response.cn_mean;
 				if(mean){
 					msg = mean.replace('\n', '<br>');
@@ -12,7 +13,7 @@
 				else{
 					msg= '<font color="red">未找到该单词.</font>';
 				}
-				layer.alert(msg,{shadeClose: true,closeBtn: 1, yes: function (index) { 
+				layer.alert(msg,{title: str, shadeClose: true,closeBtn: 1, btn:['详情'], detail: function (index) { 
 					window.open('http://www.iciba.com/'+str);
 					layer.close(index);
 				}});
